91428b3edf6a94641d2ff6bd98d219ce720dba64
Stan Vlad <vstan02@protonmail.com>
2021-01-31 20:23:04 +0200

    I added sys_time function

--- demo/src/main.asm
+++ demo/src/main.asm
@@ -1,12 +1,13 @@
 global _start
 
+extern sys_time
 extern int_print
 extern eol_print
 extern sys_exit
 
 section .text
 	_start:
-		mov rax, -575
+		call sys_time
 		call int_print
 		call eol_print
 		call sys_exit
